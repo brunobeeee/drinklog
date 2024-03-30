272ef96050ead117fe36011a328262f323c1288c
brunobeeee <github@bbergmann.de>
2024-03-30 15:00:47 +0100

    Create first db list view

--- app/backend/views.py
+++ app/backend/views.py
@@ -5,6 +5,3 @@ from drinklog.models import Log
 # Create your views here.
 class LogList(ListView):
     model = Log
-    #template_name = './templates/log_list.html'
-    context_object_name = 'logs'
-    ordering = ['-date']
