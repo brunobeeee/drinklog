d3507e6a1048fcd0f25a299154640b1abbd49dec
brunobeeee <github@bbergmann.de>
2024-04-02 17:56:08 +0200

    Import ToDo-List template from @divanov11

--- app/drinklog/wsgi.py
+++ app/drinklog/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for example project.
+WSGI config for drinklog project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drinklog.settings')
 
 application = get_wsgi_application()
