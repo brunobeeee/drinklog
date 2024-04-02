f03add94e38f145a67909420d4878e74e36c28ab
brunobeeee <github@bbergmann.de>
2024-04-02 18:00:55 +0200

    Add Log db model to models.py

--- app/base/models.py
+++ app/base/models.py
@@ -1,7 +1,26 @@
+import os
+
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
 
+class Log(models.Model):
+    user = models.ForeignKey(User,
+        on_delete=models.CASCADE,
+        null=os.environ.get("ALLOW_NULL_USERS"),
+        blank=os.environ.get("ALLOW_BLANK_USERS"))
+    date = models.DateField(auto_now_add=True)
+    intensity = models.IntegerField(default=0)
+    overdrive = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.date) + " | " + str(self.intensity) + " | " + str(self.overdrive)
+
+    class Meta:
+        ordering = ['date']
+        order_with_respect_to = 'user'
+        verbose_name = 'Log'
+        verbose_name_plural = 'Logs'
+        db_table = 'log'
 
 class Task(models.Model):
     user = models.ForeignKey(
