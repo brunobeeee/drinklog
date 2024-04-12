ad2331805e068439cae91689c526fc76c6aea2a9
n-koenig <30596454+n-koenig@users.noreply.github.com>
2024-04-12 18:59:02 +0200

    Workflows (#7)
    * Add plotly to the project and a first interactive bar chart view of the logs
    
    Co-authored-by: Bruno <brunobeeee@users.noreply.github.com>
    
    * Added pr template and formatting workflow
    
    * modified:   .github/pull_request_template.md
    
    * attempt to fix broken workflow
    
    * still trying to fix the workflow
    
    * maybe this will work?
    
    * lets see
    
    * Automation: Code Formatter (api)
    
    ---------
    
    Co-authored-by: Bruno <brunobeeee@users.noreply.github.com>
    Co-authored-by: github-actions <github-actions@github.com>

--- app/base/views.py
+++ app/base/views.py
@@ -1,42 +1,42 @@
-from django.shortcuts import render, redirect
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
-from django.urls import reverse_lazy
+from datetime import datetime, timedelta
 
-from django.contrib.auth.views import LoginView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.forms import UserCreationForm
+import pandas as pd
+import plotly.express as px
 from django.contrib.auth import login
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
+from django.db import transaction
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
 # Imports for Reordering Feature
 from django.views import View
-from django.shortcuts import redirect
-from django.db import transaction
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import (CreateView, DeleteView, FormView,
+                                       UpdateView)
+from django.views.generic.list import ListView
 
-from .models import Log
 from .forms import PositionForm
-
-from datetime import datetime, timedelta
+from .models import Log
 
 import plotly.express as px
 import pandas as pd
 
 
 class CustomLoginView(LoginView):
-    template_name = 'base/login.html'
-    fields = '__all__'
+    template_name = "base/login.html"
+    fields = "__all__"
     redirect_authenticated_user = True
 
     def get_success_url(self):
-        return reverse_lazy('logs')
+        return reverse_lazy("logs")
 
 
 class RegisterPage(FormView):
-    template_name = 'base/register.html'
+    template_name = "base/register.html"
     form_class = UserCreationForm
     redirect_authenticated_user = True
-    success_url = reverse_lazy('logs')
+    success_url = reverse_lazy("logs")
 
     def form_valid(self, form):
         user = form.save()
@@ -46,70 +46,73 @@ class RegisterPage(FormView):
 
     def get(self, *args, **kwargs):
         if self.request.user.is_authenticated:
-            return redirect('logs')
+            return redirect("logs")
         return super(RegisterPage, self).get(*args, **kwargs)
 
 
 class LogList(LoginRequiredMixin, ListView):
     model = Log
-    context_object_name = 'logs'
+    context_object_name = "logs"
 
     def get_queryset(self):
         queryset = super().get_queryset()
         queryset = queryset.filter(user=self.request.user)
 
-        search_input = self.request.GET.get('search-area') or ''
+        search_input = self.request.GET.get("search-area") or ""
         if search_input:
             queryset = queryset.filter(date__contains=search_input)
 
-        return queryset.order_by('-date')  # Sortiere nach Datum absteigend
+        return queryset.order_by("-date")  # Sortiere nach Datum absteigend
+
 
 class LogDetail(LoginRequiredMixin, DetailView):
     model = Log
-    context_object_name = 'log'
-    template_name = 'base/log.html'
+    context_object_name = "log"
+    template_name = "base/log.html"
 
 
 class LogCreate(LoginRequiredMixin, CreateView):
     model = Log
-    fields = ['date', 'intensity', 'overdrive']
-    success_url = reverse_lazy('logs')
+    fields = ["date", "intensity", "overdrive"]
+    success_url = reverse_lazy("logs")
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(LogCreate, self).form_valid(form)
 
     def get_initial(self):
-            initial = super().get_initial()
-            initial['date'] = datetime.now() - timedelta(days=1)
-            return initial
+        initial = super().get_initial()
+        initial["date"] = datetime.now() - timedelta(days=1)
+        return initial
 
 
 class LogUpdate(LoginRequiredMixin, UpdateView):
     model = Log
-    fields = ['date', 'intensity', 'overdrive']
-    success_url = reverse_lazy('logs')
+    fields = ["date", "intensity", "overdrive"]
+    success_url = reverse_lazy("logs")
 
 
 class DeleteView(LoginRequiredMixin, DeleteView):
     model = Log
-    context_object_name = 'log'
-    success_url = reverse_lazy('logs')
+    context_object_name = "log"
+    success_url = reverse_lazy("logs")
+
     def get_queryset(self):
         owner = self.request.user
         return self.model.objects.filter(user=owner)
+
 
 class LogReorder(View):
     def post(self, request):
         form = PositionForm(request.POST)
 
         if form.is_valid():
-            positionList = form.cleaned_data["position"].split(',')
+            positionList = form.cleaned_data["position"].split(",")
 
             with transaction.atomic():
                 self.request.user.set_log_order(positionList)
 
-        return redirect(reverse_lazy('logs'))
+        return redirect(reverse_lazy("logs"))
 
 
 def logplot(request):
@@ -117,8 +120,14 @@ def logplot(request):
     dates = [log.date for log in all_logs]
     intensities = [log.intensity for log in all_logs]
 
-    df = pd.DataFrame({'date': dates, 'intensity': intensities})
-    fig_bar = px.bar(df, x='date', y='intensity', title="Drinklog Plot", labels={'intensity': 'Intensity', 'date': 'Date'})
+    df = pd.DataFrame({"date": dates, "intensity": intensities})
+    fig_bar = px.bar(
+        df,
+        x="date",
+        y="intensity",
+        title="Drinklog Plot",
+        labels={"intensity": "Intensity", "date": "Date"},
+    )
     bar_chart = fig_bar.to_html(full_html=False, include_plotlyjs=False)
 
     return render(request, "base/log_plot.html", {"bar_chart": bar_chart})
--- app/drinklog/wsgi.py
+++ app/drinklog/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drinklog.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "drinklog.settings")
 
 application = get_wsgi_application()
