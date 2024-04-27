1c20414b8b32b96ea6fb0a41c762885faa26af9e
brunobeeee <github@bbergmann.de>
2024-04-27 16:50:51 +0200

    feat: add a DOMContentLoaded event listener before rendering video bg

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,15 +1,18 @@
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from 'bootstrap';
 import 'youtube-background';
 
-const videoBackgrounds = new VideoBackgrounds('[data-vbg]', {
-    'inline-styles': true
+document.addEventListener("DOMContentLoaded", function() {
+
+    const videoBackgrounds = new VideoBackgrounds('[data-vbg]', {
+        'inline-styles': true
+    });
+
+    const videoElement = document.querySelector('[data-vbg]');
+
+    if (videoElement) {
+        // get the first instance instance by UID
+        const videoInstance = videoBackgrounds.get(videoElement);
+        // add the element to the factory instance
+        videoBackgrounds.add(videoElement);
+    }
 });
-
-const videoElement = document.querySelector('[data-vbg]');
-
-if (videoElement) {
-    // get the first instance instance by UID
-    const videoInstance = videoBackgrounds.get(videoElement);
-    // add the element to the factory instance
-    videoBackgrounds.add(videoElement);
-}
