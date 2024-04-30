6ffba93a82d57a91ce325c9d268f50364bfec80b
brunobeeee <github@bbergmann.de>
2024-04-30 18:50:02 +0200

    Add a black to the colorscheme and use it for overdrive bars

--- app/base/views.py
+++ app/base/views.py
@@ -109,22 +109,34 @@ class DeleteView(LoginRequiredMixin, DeleteView):
 
 def logplot(request):
     current_user = request.user
+    
     all_logs = Log.objects.all()
     users = [log.user for log in all_logs]
     dates = [log.date for log in all_logs]
     intensities = [log.intensity for log in all_logs]
+    overdrives = [log.overdrive for log in all_logs]
 
-    df = pd.DataFrame({"user": users, "date": dates, "intensity": intensities})
+    # Create df
+    df = pd.DataFrame({"user": users, "date": dates, "intensity": intensities, "overdrive": overdrives})
     df_current_user = df[df["user"] == current_user]
 
+    # Add color column with black values when overdrive==True
+    df_current_user['color'] = df_current_user.apply(lambda row: 0 if row['overdrive'] else row['intensity'], axis=1)
+
+    print(df_current_user['color'])
+
+    # Erstellen des Barplots
     fig_bar = px.bar(
         df_current_user,
         x="date",
         y="intensity",
         title="Drinklog Plot",
         labels={"intensity": "Intensity", "date": "Date"},
-        color="intensity",
-        color_continuous_scale=["#00B86B", "#D78F09", "#CD133B"],
+        color = df_current_user["color"],
+        color_continuous_scale=["black",
+            "#00B86B", "#36AE53", "#6CA43A", "#A29A22", 
+            "#D78F09", "#D57016", "#D25122", "#D0322F",
+            "#CD133B",],
     )
     fig_bar.update_layout(
         paper_bgcolor="rgba(0,0,0,0)",
